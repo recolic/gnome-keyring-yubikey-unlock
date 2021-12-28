32561916ec636f75bbc9c442577c8c0d0a1e1a42
Sebastian Wieland <git-commits@nwie.land>
2021-12-28 11:01:01 +0100

    Automatic pinentry

--- unlock_keyrings.sh
+++ unlock_keyrings.sh
@@ -3,8 +3,9 @@
 
 _self_bin_name="$0"
 secret_file="$1"
+smartcard_pin="$2"
 
-[[ "$secret_file" = '' ]] && echo "Usage: $0 <secret_file>" && exit 1
+[[ "$secret_file" = '' ]] && echo "Usage: $0 <secret_file> [<smartcard pin>]" && exit 1
 
 function where_is_him () {
     SOURCE="$1"
@@ -22,8 +23,13 @@ function where_am_i () {
     [[ "$_my_path" = "" ]] && where_is_him "$_self_bin_name" || where_is_him "$_my_path"
 }
 
+gpg_options=()
+if [[ ! "$smartcard_pin" = '' ]]; then
+    gpg_options=("--pinentry-mode" "loopback" "--passphrase" "$smartcard_pin")
+fi
+
 cd `where_am_i` &&
-gpg --decrypt "$secret_file" | bin/unlock_keyrings --secret-file - --quiet
+gpg "${gpg_options[@]}" --decrypt "$secret_file" | bin/unlock_keyrings --secret-file - --quiet
 
 exit $?
 
